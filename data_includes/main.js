f66754dfe7c49a973bc549ee8bec77c0f0ed8a16
Susanne Flach <susanne.flach@unine.ch>
2020-12-02 15:46:37 +0100

    Update order

--- data_includes/main.js
+++ data_includes/main.js
@@ -12,7 +12,12 @@ PennController.ResetPrefix(null) // Shorten command names (keep this line here)
 // After that, send the results and finally show the trial labeled 'bye'.
 //Sequence("intro", "instructions", "training", "intermission", sepWithN( "break" , randomize("experiment") , 4), SendResults(), "goodbye")
 Sequence("intro",
-    "experiment", SendResults(), "goodbye")
+    "instructions",
+    "training",
+    "intermission",
+    sepWithN("break", randomize("experiment"), 6),
+    SendResults(),
+    "goodbye")
 
 // What is in Header happens at the beginning of every single trial
 Header(
@@ -79,6 +84,61 @@ newTrial("instructions" ,
         .print()
         .wait()
 ) // instructions
+
+Template("training.csv", row =>
+        newTrial("training",
+
+            newText("Sentence", row.Sentence)
+                .css("font-size", "1.2em")
+                .css("font-family", "Verdana")
+                .print()
+                .log()
+            ,
+            newScale("Judgement", "yes", "no")
+                .settings.radio()
+                .settings.labelsPosition("bottom")
+                .settings.center()
+                .print()
+                .settings.log()
+                .wait("")
+            ,
+            newTimer(500)
+                .start()
+                .wait()
+        )
+            // logs additional variables in sentence file (e.g., Fun)
+            .log("Id", row.Id)
+            .log("Sentence", row.Sentence)
+            .log("Target", row.Target)
+            .log("Condition", row.Condition)
+            .log("Corr", row.Corr)
+    //.log("judgement")
+    )
+
+// Intermission
+newTrial("intermission",
+
+    newText("<p>Well done, you should be good to go.<br/>" +
+        "Remember: try to be quick <strong>and</strong> accurate.</p>" +
+        "<p>You are now going to judge 42 sentences.<br/>" +
+        "There will be a designated break every 14 sentences, if you want.<br/></p>")
+        .css("font-family", "Verdana")
+        .print()
+    ,
+    newText("(Please do not take a break <em>while</em> reading/judging a sentence.)")
+        .print()
+    ,
+    newText("<p>Click OK when you are ready to proceed to the main experiment.</p>")
+        .css("font-size", "1em")
+        .css("font-family", "Verdana")
+        .print()
+    ,
+    newButton("OK")
+        .size(100)
+        .center()
+        .print()
+        .wait()
+)
 
 Template("main.csv", row =>
     newTrial("experiment",
